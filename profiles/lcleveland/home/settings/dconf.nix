e85bb2b5444b71f2031e28f0c57863b2451bd69d
Lyle Cleveland <lyle.cleveland@yahoo.com>
2024-07-07 20:02:14 -0500

    formatting

--- profiles/lcleveland/home/settings/dconf.nix
+++ profiles/lcleveland/home/settings/dconf.nix
@@ -1,13 +1,16 @@
 { pkgs, settings, ... }: {
   home-manager.users.${settings.username}.dconf = {
     enable = true;
-    settings."org/gnome/shell" = {
-      enabled-extensions = with pkgs.gnomeExtensions; [
-        blur-my-shell.extensionUuid
-        gsnap.extensionUuid
-        dash-to-panel.extensionUuid
-        appindicator.extensionUuid
-      ];
+    settings = {
+      "org/gnome/shell" = {
+        enabled-extensions = with pkgs.gnomeExtensions; [
+          blur-my-shell.extensionUuid
+          gsnap.extensionUuid
+          dash-to-panel.extensionUuid
+          appindicator.extensionUuid
+        ];
+        "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+      };
     };
   };
 }
