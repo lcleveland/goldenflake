f3a8986b542d1d9cef051ee7acb09dc27ea87d95
Lyle Cleveland <lyle.cleveland@proton.me>
2024-07-09 09:23:02 -0500

    fix pypAm

--- profiles/lcleveland/home/applications/pynvim.nix
+++ profiles/lcleveland/home/applications/pynvim.nix
@@ -2,6 +2,7 @@
   home-manager.users.${settings.username}.home.packages = with pkgs; [
     (python3.withPackages (python-pkgs: [
       python312Packages.pynvim
+      python312Packages.pypamtest
     ]))
   ];
 }
