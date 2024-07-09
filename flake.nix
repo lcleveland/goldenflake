678fb0de4e5fc6e970eb67e914c14a781fbf927f
Lyle Cleveland <lyle.cleveland@proton.me>
2024-07-09 10:39:58 -0500

    add nixvim

--- flake.nix
+++ flake.nix
@@ -46,5 +46,9 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 }
