ab2688590e8f4579248b9f126eed813426197843
Lyle Cleveland <lyle.cleveland@proton.me>
2024-07-09 07:40:19 -0500

    add triple buffering

--- system/settings/desktop_environment.nix
+++ system/settings/desktop_environment.nix
@@ -27,5 +27,20 @@
     gnome-characters
     gnome-music
   ]);
-
+  nixpkgs.overlays = [
+    (final: prev: {
+      gnome = prev.gnome.overrideScope (gnomeFinal: gnomePrev: {
+        mutter = gnomePrev.mutter.overrideAttrs (old: {
+          src = pkgs.fetchFromGitLab {
+            domain = "gitlab.gnome.org";
+            owner = "vanvugt";
+            repo = "mutter";
+            rev = "triple-buffering-v4-46";
+            hash = "sha256-fkPjB/5DPBX06t7yj0Rb3UEuu5b9mu3aS+jhH18+lpI=";
+          };
+        });
+      });
+    })
+  ];
+  nixpkgs.config.allowAliases = false;
 }
