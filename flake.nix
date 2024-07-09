1bd856e41e51073b67253bb79bb1bc3219f47bce
Lyle Cleveland <lyle.cleveland@proton.me>
2024-07-09 10:38:17 -0500

    format update

--- flake.nix
+++ flake.nix
@@ -42,7 +42,9 @@
     };
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 }
