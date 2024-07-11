fc75e6ffd911205b5711ba8c4f578a5073dab9ec
Lyle Cleveland <lyle.cleveland@proton.me>
2024-07-11 14:20:38 -0500

    remove old packages

--- profiles/lcleveland/home/applications/default.nix
+++ profiles/lcleveland/home/applications/default.nix
@@ -1,16 +1,11 @@
 { ... }: {
   imports = [
     ./gsnap.nix
-    ./bottom.nix
     ./fira_code_nerdfont.nix
     ./icedteam_web.nix
     ./gdu.nix
     ./l2tp.nix
     ./lazygit.nix
-    ./nodejs.nix
-    ./ripgrep.nix
-    ./pynvim.nix
-    ./tree_sitter.nix
     ./rustup.nix
     ./unzip.nix
   ];
