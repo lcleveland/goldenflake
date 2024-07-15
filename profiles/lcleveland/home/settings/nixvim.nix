1165354f72773ba7388b0b8b02cbed78195fcbf1
Lyle Cleveland <lyle.cleveland@proton.me>
2024-07-15 08:55:35 -0500

    change view

--- profiles/lcleveland/home/settings/nixvim.nix
+++ profiles/lcleveland/home/settings/nixvim.nix
@@ -57,8 +57,8 @@
         };
         noice = {
           enable = true;
-          presets = {
-            command_palette = true;
+          cmdline = {
+            view = "cmdline";
           };
         };
         notify = {
