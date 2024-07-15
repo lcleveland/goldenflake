d8e1137ee461fec32948466749e17146f3fe3760
Lyle Cleveland <lyle.cleveland@proton.me>
2024-07-15 08:41:31 -0500

    add sources

--- profiles/lcleveland/home/settings/nixvim.nix
+++ profiles/lcleveland/home/settings/nixvim.nix
@@ -34,6 +34,15 @@
               {
                 name = "nvim_lsp";
               }
+              {
+                name = "buffer";
+              }
+              {
+                name = "path";
+              }
+              {
+                name = "cmdline";
+              }
             ];
           };
         };
