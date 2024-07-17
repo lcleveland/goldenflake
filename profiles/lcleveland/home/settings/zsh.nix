f2dda7207371c438cbb634fda3a8ae2ccc5c0def
Lyle Cleveland <lyle.cleveland@proton.me>
2024-07-17 11:31:53 -0500

    add zsh

--- profiles/lcleveland/home/settings/zsh.nix
+++ profiles/lcleveland/home/settings/zsh.nix
@@ -1,13 +1,14 @@
-{ settings, config, ... }: {
+{ settings, ... }: {
 
   home-manager.users.${settings.username}.programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+    enableVteIntegration = true;
     history = {
-      size = 1000;
-      path = "${config.xdg.dataHome}/zsh/history";
+      size = 10000;
+      path = "/home/${settings.username}/.config/zsh/history";
     };
   };
 }
