2fe7842fb294381d2e3fd75fc4d4de7890679199
Lyle Cleveland <lyle.cleveland@proton.me>
2024-07-17 11:37:54 -0500

    fix

--- profiles/lcleveland/home/settings/zsh.nix
+++ profiles/lcleveland/home/settings/zsh.nix
@@ -1,14 +1,17 @@
 { settings, pkgs, ... }: {
   users.users.${settings.username}.shell = pkgs.zsh;
-  home-manager.users.${settings.username}.programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-    enableVteIntegration = true;
-    history = {
-      size = 10000;
-      path = "/home/${settings.username}/.config/zsh/history";
+  home-manager.users.${settings.username} = {
+    shell = pkgs.zsh;
+    programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      enableVteIntegration = true;
+      history = {
+        size = 10000;
+        path = "/home/${settings.username}/.config/zsh/history";
+      };
     };
   };
 }
