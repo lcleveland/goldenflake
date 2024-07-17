cc6e136f831e663355c432612443087c060fb7de
Lyle Cleveland <lyle.cleveland@proton.me>
2024-07-17 12:39:26 -0500

    fix

--- profiles/lcleveland/home/settings/tmux.nix
+++ profiles/lcleveland/home/settings/tmux.nix
@@ -3,7 +3,7 @@
   home-manager.users.${settings.username}.programs.tmux = {
     enable = true;
     extraConfig = ''
-      set-option -g default-shell ${pkgs.${settings.default_shell}}
+      set-option -g default-shell ${pkgs.${settings.default_shell}}/bin/${settings.default_shell}
       set-window-option -g mode-keys vi
       bind h select-pane -L
       bind j select-pane -D
