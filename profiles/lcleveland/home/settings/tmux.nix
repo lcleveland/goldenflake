fdf767439c1385c14242309fbade6bc91df9b279
Lyle Cleveland <lyle.cleveland@proton.me>
2024-07-17 11:16:24 -0500

    bindings

--- profiles/lcleveland/home/settings/tmux.nix
+++ profiles/lcleveland/home/settings/tmux.nix
@@ -3,6 +3,7 @@
   home-manager.users.${settings.username}.programs.tmux = {
     enable = true;
     extraConfig = ''
+      set-window-option -g mode-keys vi
       bind h select-pane -L
       bind j select-pane -D
       bind k select-pane -U
