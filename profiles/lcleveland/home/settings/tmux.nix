c250bfe7443ce7d007669a370fba58aaa289834f
Lyle Cleveland <lyle.cleveland@proton.me>
2024-07-17 11:11:44 -0500

    vim keybinds

--- profiles/lcleveland/home/settings/tmux.nix
+++ profiles/lcleveland/home/settings/tmux.nix
@@ -3,7 +3,10 @@
   home-manager.users.${settings.username}.programs.tmux = {
     enable = true;
     extraConfig = ''
-      set-window-option -g mode-keys vi
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
     '';
   };
 }
