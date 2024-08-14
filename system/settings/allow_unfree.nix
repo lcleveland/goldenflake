1bde50e31eb6af51f6e1e87523b81d317babda26
Lyle Cleveland <lyle.cleveland@proton.me>
2024-08-13 19:48:54 -0500

    test

--- system/settings/allow_unfree.nix
+++ system/settings/allow_unfree.nix
@@ -1,3 +1,18 @@
-{ ... }: {
-  nixpkgs.config.allowUnfree = true;
+{ lib, config, ... }:
+with lib;
+let
+  cfg = config.system.applications.settings.allow_unfree;
+in
+{
+  options.system.settings.applications.settings.allow_unfree = {
+    enable = mkOption
+      {
+        type = types.bool;
+        default = true;
+      };
+  };
+
+  config = mkIf cfg.enable {
+    allowUnfree = cfg.enable;
+  };
 }
