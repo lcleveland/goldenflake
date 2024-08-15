0064a4fce45f0689c0d476a5989ba1cb0b1f04e7
Lyle Cleveland <lyle.cleveland@proton.me>
2024-08-14 20:23:01 -0500

    change to use option

--- system/settings/fingerprint.nix
+++ system/settings/fingerprint.nix
@@ -1,3 +1,16 @@
-{ settings, ... }: {
-  services.fprintd.enable = settings.enable_fingerprint;
+{ lib, config, ... }:
+let
+  cfg = config.system.settings.fingerprint;
+in
+{
+  options.system.settings.fingerprint = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = lib.mdDoc "Enable fingerprinting";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    services.fprintd.enable = cfg.enable;
+  };
 }
