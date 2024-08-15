d8c53662f70426830b339c429626698455d3ae45
Lyle Cleveland <lyle.cleveland@proton.me>
2024-08-15 10:50:27 -0500

    change flatpak to use options

--- system/settings/flatpak.nix
+++ system/settings/flatpak.nix
@@ -1,9 +1,32 @@
-{ pkgs, ... }: {
-  services.flatpak.enable = true;
-  environment.etc = {
-  "flatpak/remotes.d/flathub.flatpakrepo".source = pkgs.fetchurl {
-  url = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-  hash = "sha256-M3HdJQ5h2eFjNjAHP+/aFTzUQm9y9K+gwzc64uj+oDo=";
+{ pkgs, lib, config, ... }:
+let
+  cfg = config.system.settings.flatpak;
+in
+{
+  options.system.settings.flatpak = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = lib.mdDoc "Enable Flatpak";
+    };
+    repo_hash = lib.mkOption {
+      type = lib.types.str;
+      default = "sha256-M3HdJQ5h2eFjNjAHP+/aFTzUQm9y9K+gwzc64uj+oDo=";
+      description = lib.mdDoc "Flatpak repo hash";
+    };
+    repo_url = lib.mkOption {
+      type = lib.types.str;
+      default = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+      description = lib.mdDoc "Flatpak repo url";
+    };
   };
+  config = lib.mkIf cfg.enable {
+    services.flatpak.enable = cfg.enable;
+    environment.etc = {
+      "flatpak/remotes.d/flathub.flatpakrepo".source = pkgs.fetchurl {
+        url = cfg.repo_url;
+        hash = cfg.repo_hash;
+      };
+    };
   };
 }
