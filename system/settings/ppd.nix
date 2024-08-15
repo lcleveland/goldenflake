9d8c153551ec7356a105440156bc3e5b70f1fe7d
Lyle Cleveland <lyle.cleveland@proton.me>
2024-08-15 13:35:20 -0500

    change to use options

--- system/settings/ppd.nix
+++ system/settings/ppd.nix
@@ -1,3 +1,16 @@
-{ ... }: {
-  services.power-profiles-daemon.enable = true;
+{ lib, config, ... }:
+let
+  cfg = config.system.settings.ppd;
+in
+{
+  options.system.settings.ppd = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      description = "Enable PPD";
+      default = true;
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    services.power-profiles-daemon.enable = cfg.enable;
+  };
 }
