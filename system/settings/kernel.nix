de9fc1858abbcb32d9f0f8203c824dbe0830e928
Lyle Cleveland <lyle.cleveland@proton.me>
2024-08-15 11:57:22 -0500

    change to use option

--- system/settings/kernel.nix
+++ system/settings/kernel.nix
@@ -1,3 +1,16 @@
-{ pkgs, ... }: {
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+{ pkgs, lib, config, ... }:
+let
+  cfg = config.system.settings.kernel;
+in
+{
+  options.system.settings.kernel = {
+    kernel_pkg = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.linuxPackages_latest;
+      description = "The kernel package to use";
+    };
+  };
+  config = {
+    boot.kernelPackages = cfg.kernel_pkg;
+  };
 }
