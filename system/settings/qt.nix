f88da206b6100e867e4126f79b987670b1a73863
Lyle Cleveland <lyle.cleveland@proton.me>
2024-08-15 13:40:49 -0500

    change to use options

--- system/settings/qt.nix
+++ system/settings/qt.nix
@@ -1,3 +1,21 @@
-{ settings, ... }: {
-  qt.style = settings.qt_style;
+{ lib, config, ... }:
+let
+  cfg = config.system.settings.qt;
+in
+{
+  options.system.settings.qt = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable QT";
+    };
+    style = lib.mkOption {
+      type = lib.types.str;
+      default = "adwaita-dark";
+      description = "QT style";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    qt.style = cfg.style;
+  };
 }
