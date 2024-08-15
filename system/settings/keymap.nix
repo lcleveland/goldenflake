f9d257beb57ad4e1d6b4870412cdb1db66ece4f1
Lyle Cleveland <lyle.cleveland@proton.me>
2024-08-15 08:41:16 -0500

    change to use options

--- system/settings/keymap.nix
+++ system/settings/keymap.nix
@@ -1,6 +1,24 @@
-{ settings, ... }: {
-  services.xserver.xkb = {
-    layout = settings.keyboard.layout;
-    variant = settings.keyboard.variant;
+{ lib, config, ... }:
+let
+  cfg = config.system.settings.keymap;
+in
+{
+  options.system.settings.keymap = {
+    layout = lib.mkOption {
+      type = lib.types.str;
+      default = "us";
+      description = "The keyboard layout to use";
+    };
+    variant = lib.mkOption {
+      type = lib.types.str;
+      default = "";
+      description = "The keyboard variant to use";
+    };
+  };
+  config = {
+    services.xserver.xkb = {
+      layout = cfg.layout;
+      variant = cfg.variant;
+    };
   };
 }
