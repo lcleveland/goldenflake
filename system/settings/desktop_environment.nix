ff76846d053161853daf719e9eba307f485a9fc8
Lyle Cleveland <lyle.cleveland@proton.me>
2024-08-14 19:23:48 -0500

    change to support multiple DE and move to options

--- system/settings/desktop_environment.nix
+++ system/settings/desktop_environment.nix
@@ -1,47 +1,26 @@
-{ pkgs, ... }: {
-  services = {
-    xserver = {
-      excludePackages = with pkgs; [
-        xterm
-      ];
-      desktopManager = {
-        gnome = {
-          enable = true;
-        };
-        xterm.enable = false;
-      };
+{ lib, config, ... }:
+with lib;
+let
+  cfg = config.system.settings.desktop_environment;
+in
+{
+  options.system.settings.desktop_environment = {
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = lib.mdDoc ''
+        Enable desktop environment;
+      '';
     };
-    udev.packages = with pkgs.gnome; [ gnome-settings-daemon ];
+    environment = {
+      type = types.str;
+      default = "gnome";
+      description = lib.mdDoc ''
+        Environment to use;
+      '';
+    };
   };
-  environment.gnome.excludePackages = (with pkgs; [
-    gnome-tour
-    gnome-terminal
-    gnome-console
-    gnome-connections
-    gnome-text-editor
-    gedit
-    epiphany
-    geary
-    evince
-    totem
-  ]) ++ (with pkgs.gnome; [
-    gnome-characters
-    gnome-music
-  ]);
-  nixpkgs.overlays = [
-    (final: prev: {
-      gnome = prev.gnome.overrideScope (gnomeFinal: gnomePrev: {
-        mutter = gnomePrev.mutter.overrideAttrs (old: {
-          src = pkgs.fetchFromGitLab {
-            domain = "gitlab.gnome.org";
-            owner = "vanvugt";
-            repo = "mutter";
-            rev = "triple-buffering-v4-46";
-            hash = "sha256-C2VfW3ThPEZ37YkX7ejlyumLnWa9oij333d5c4yfZxc=";
-          };
-        });
-      });
-    })
-  ];
-  nixpkgs.config.allowAliases = false;
+  config = mkIf cfg.enable {
+    config.system.settings.desktop_environment.${cfg.environment}.enable = true;
+  };
 }
---
+++ system/settings/desktop_environments/gnome/default.nix
@@ -0,0 +1,63 @@
+{ pkgs, lib, config, ... }:
+with lib;
+let
+  cfg = config.system.settings.desktop_environment.gnome;
+in
+{
+  options.system.settings.desktop_environment.gnome = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = lib.mdDoc ''
+        Enable desktop environment
+      '';
+    };
+  };
+  config = mkIf cfg.enable {
+    services = {
+      xserver = {
+        excludePackages = with pkgs; [
+          xterm
+        ];
+        desktopManager = {
+          gnome = {
+            enable = true;
+          };
+          xterm.enable = false;
+        };
+      };
+      udev.packages = with pkgs.gnome; [ gnome-settings-daemon ];
+    };
+    environment.gnome.excludePackages = (with pkgs; [
+      gnome-tour
+      gnome-terminal
+      gnome-console
+      gnome-connections
+      gnome-text-editor
+      gedit
+      epiphany
+      geary
+      evince
+      totem
+    ]) ++ (with pkgs.gnome; [
+      gnome-characters
+      gnome-music
+    ]);
+    nixpkgs.overlays = [
+      (final: prev: {
+        gnome = prev.gnome.overrideScope (gnomeFinal: gnomePrev: {
+          mutter = gnomePrev.mutter.overrideAttrs (old: {
+            src = pkgs.fetchFromGitLab {
+              domain = "gitlab.gnome.org";
+              owner = "vanvugt";
+              repo = "mutter";
+              rev = "triple-buffering-v4-46";
+              hash = "sha256-C2VfW3ThPEZ37YkX7ejlyumLnWa9oij333d5c4yfZxc=";
+            };
+          });
+        });
+      })
+    ];
+    nixpkgs.config.allowAliases = false;
+  };
+}
