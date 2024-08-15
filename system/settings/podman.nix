d8c7f81305358f968dfc8923cae59908776f7e26
Lyle Cleveland <lyle.cleveland@proton.me>
2024-08-15 13:29:18 -0500

    change to use options

--- system/settings/podman.nix
+++ system/settings/podman.nix
@@ -1,9 +1,22 @@
-{ pkgs, ... }: {
-  virtualisation = {
-    podman = {
-      enable = true;
-      dockerCompat = true;
-      defaultNetwork.settings.dns_enabled = true;
+{ lib, config, ... }:
+let
+  cfg = config.system.settings.podman;
+in
+{
+  options.system.settings.podman = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = lib.mdDoc "Enable podman";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    virtualisation = {
+      podman = {
+        enable = cfg.enable;
+        dockerCompat = true;
+        defaultNetwork.settings.dns_enabled = true;
+      };
     };
   };
 }
