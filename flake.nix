07a48da5514118dd7d06cf07b88ecf8a3519ecea
lcleveland <lyle.cleveland@proton.me>
2024-08-16 08:16:42 -0500

    remove unused

--- flake.nix
+++ flake.nix
@@ -9,7 +9,6 @@
         default_editor = "neovim";
         default_terminal = "kitty";
         default_shell = "zsh";
-        profile = "home";
         gtk_theme = "Adwaita-dark";
       };
     in
