d5d22df05648c9d41abb1221ebe150c062223f03
lcleveland <lyle.cleveland@proton.me>
2024-08-17 08:42:52 -0500

    add plugins support and clean up vi mode

--- users/lcleveland/home/settings/tmux.nix
+++ users/lcleveland/home/settings/tmux.nix
@@ -1,15 +1,15 @@
-{ lib, config, pkgs, settings, ... }: {
+{ lib, config, pkgs, ... }: {
   config = lib.mkIf config.users.lcleveland.enable {
     home-manager.users.${config.users.lcleveland.username}.programs.tmux = {
       enable = true;
+      keyMode = "vi";
+      mouse = true;
       extraConfig = ''
         set-option -g default-shell ${pkgs.${config.users.lcleveland.default_shell}}/bin/${config.users.lcleveland.default_shell}
-        set-window-option -g mode-keys vi
-        bind h select-pane -L
-        bind j select-pane -D
-        bind k select-pane -U
-        bind l select-pane -R
       '';
+      plugins = with pkgs.tmuxPlugins; [
+        tokyo-night-tmux
+      ];
     };
   };
 }
