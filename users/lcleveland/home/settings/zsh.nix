edcdabf8b41acaf67dda584dacc9c99b18b204e7
lcleveland <lyle.cleveland@proton.me>
2024-08-17 19:36:12 -0500

    stop using plugin

--- users/lcleveland/home/settings/zsh.nix
+++ users/lcleveland/home/settings/zsh.nix
@@ -6,19 +6,9 @@
       enable = true;
       enableCompletion = true;
       syntaxHighlighting.enable = true;
+      autosuggestion.enable = true;
       enableVteIntegration = true;
       history.ignoreAllDups = true;
-      plugins = [
-        {
-          name = "zsh-autosuggestions";
-          src = pkgs.fetchFromGitHub {
-            owner = "zsh-users";
-            repo = "zsh-autosuggestions";
-            rev = "v0.7.0";
-            sha256 = "sha256-KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w=";
-          };
-        }
-      ];
       oh-my-zsh =
         {
           enable = true;
