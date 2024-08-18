4c71aa7afdc07bfe1f222b877b30b07d725c18b4
lcleveland <lyle.cleveland@proton.me>
2024-08-17 19:31:10 -0500

    fix sha

--- users/lcleveland/home/settings/zsh.nix
+++ users/lcleveland/home/settings/zsh.nix
@@ -15,7 +15,7 @@
             owner = "zsh-users";
             repo = "zsh-autosuggestions";
             rev = "v0.7.0";
-            sha256 = "";
+            sha256 = "sha256-KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w=";
           };
         }
       ];
